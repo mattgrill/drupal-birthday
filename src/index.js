383aaff8feeb10c6920f060687f2e94e3a964568
Matthew Grill <matt.grill@acquia.com>
2018-02-14 10:09:12 -0800

    fixes, and magic api

--- src/index.js
+++ src/index.js
@@ -7,34 +7,49 @@ const app = express();
 const drupalapi = new DrupalAPI();
 
 const handler = async (req, res) => {
-  const accountCreationDate = await drupalapi
+  let responseData = {};
+  const { list: accounts } = await drupalapi
     .user({ name: req.params.username })
-    .then(({ list }) => ({
+    .catch(() => false);
+  if (accounts && accounts.length) {
+    const status = 200;
+    const accountCreationDetails = {
       formattedDate: format(
-        new Date(list[0].created * 1000),
-        'MMMM Do @ HH:mm',
+        new Date(accounts[0].created * 1000),
+        `MMMM Do ${new Date().getFullYear()} @ HH:mm`,
       ),
-      compareableDate: format(new Date(list[0].created * 1000), 'MM/DD'),
-    }))
-    .catch(console.error);
-  const isBirthday =
-    compareAsc(
-      format(new Date(), 'MM/DD'),
-      accountCreationDate.compareableDate,
-    ) === 1
-      ? ' 🎂 '
-      : '';
-  return res.send(
-    template.render(
-      accountCreationDate.formattedDate,
+      compareableDate: format(new Date(accounts[0].created * 1000), 'MM/DD'),
+    };
+    const isBirthday =
+      compareAsc(
+        format(new Date(), 'MM/DD'),
+        accountCreationDetails.compareableDate,
+      ) === 1
+        ? ' 🎂 '
+        : '';
+    const payload = template.render(
+      accountCreationDetails.formattedDate,
       req.params.username,
       isBirthday,
-    ),
-  );
+    );
+    responseData = { status, payload };
+  } else {
+    const status = 404;
+    const payload = template.error(req.params.username);
+    responseData = { status, payload };
+  }
+
+  return res.status(responseData.status).send(responseData.payload);
 };
 
+const apiHandler = async (req, res) => {
+  const data = await drupalapi.user({ name: req.params.username });
+  return res.send(data);
+};
+
+app.get('/api/:username', apiHandler);
 app.get('/favicon.ico', (req, res) => res.status(404));
 app.get('/:username', handler);
-app.get('/', (req, res) => res.send('You need to specify a username in the URL'));
+app.get('/', (req, res) => res.send(template.homepage()));
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
--- src/template.js
+++ src/template.js
@@ -20,4 +20,32 @@ const render = (date, username, isBirthday) => `
   </html>
 `;
 
-module.exports = { render };
+const homepage = () => `
+  <html>
+    <title>Drupal Birthday . Fun</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style>
+      ${css()}
+    </style>
+    <body>
+      <p>https://drupalbirthday.fun/{your drupal.org username}</p>
+    </body>
+  </html>
+`;
+
+const error = username => `
+  <html>
+    <title>Drupal Birthday . Fun</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style>
+      ${css()}
+    </style>
+    <body>
+      <p>Something went wrong when looking up, <b>${username}</b>. Maybe try again?</p>
+    </body>
+  </html>
+`;
+
+module.exports = { render, homepage, error };
