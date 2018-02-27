f6235bf60e6b3d0b455d31250bc2b001a711a619
Jim Birch <5177009+thejimbirch@users.noreply.github.com>
2018-02-27 10:26:35 -0600

    Adds Twitter Card, OG, and JSON-LD to home and user pages (#15)
    * Adds initial meta
    * Adds Twitter Card, OG, and JSON-LD to home and user pages
    * Updates Meta, OG, Twitter card descriptions with new markup

--- src/template.js
+++ src/template.js
@@ -1,19 +1,30 @@
 const css = require('./css');
 
 const header = title => `
-  <head>
-    <title>${title}</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <style>
-      ${css()}
-    </style>
-  </head>
+  <title>${title}</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <style>
+    ${css()}
+  </style>
 `;
 
 const render = ({ display, age, isBirthday, username }) => `
   <html>
-    ${header(username)}
+    <head>
+      ${header(username)}
+      <meta name="title" content="${title}'s Drupal Birthday" />
+      <meta name="description" content="${isBirthday || ''} ${username}${isBirthday ? ` ${isBirthday}` : ''}'s drupal.org birthday is ${display}. The account is ${age} old." />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Drupal Birthday . Fun" />
+      <meta property="og:title" content="${title}'s Drupal Birthday" />
+      <meta property="og:description" content="${isBirthday || ''} ${username}${isBirthday ? ` ${isBirthday}` : ''}'s drupal.org birthday is ${display}. The account is ${age} old." />
+      <meta property="og:url" content="https://drupalbirthday.fun/${username}" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content="${title}'s Drupal Birthday" />
+      <meta name="twitter:description" content="${isBirthday || ''} ${username}${isBirthday ? ` ${isBirthday}` : ''}'s drupal.org birthday is ${display}. The account is ${age} old." />
+      <meta name="twitter:url" content="https://drupalbirthday.fun/${username}" />
+    </head>
     <body>
       <p>${isBirthday || ''} <b>${username}</b>${
   isBirthday ? ` ${isBirthday}` : ''
@@ -30,6 +41,30 @@ const homepage = () => `
     <style>
       ${css()}
     </style>
+    <meta name="title" content="Drupal Birthday . Fun" />
+    <meta name="description" content="Find your Drupal Birthday, for Fun!" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Drupal Birthday . Fun" />
+    <meta property="og:title" content="Drupal Birthday . Fun" />
+    <meta property="og:description" content="Find your Drupal Birthday, for Fun!" />
+    <meta property="og:url" content="https://drupalbirthday.fun/" />
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Drupal Birthday . Fun" />
+    <meta name="twitter:description" content="Find your Drupal Birthday, for Fun!" />
+    <meta name="twitter:url" content="https://drupalbirthday.fun/" />
+    <script type="application/ld+json"> {
+        "@context": "http://schema.org",
+            "@type": "WebSite",
+            "url": "https://drupalbirthday.fun/",
+            "name": "Drupal Birthday . Fun",
+            "potentialAction": {
+            "@type": "SearchAction",
+                "target": "https://drupalbirthday.fun/{query}",
+                "query-input": "required"
+        }
+    } </script>
     <body>
       <p>https://drupalbirthday.fun/{your drupal.org username}</p>
     </body>
