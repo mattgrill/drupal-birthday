ebb606df04fb2801c7159f673cb92a24f15a68a5
Matthew Grill <matthew.grill@docusign.com>
2019-10-29 11:54:10 +0100

    🎉

---
+++ api/index.js
@@ -0,0 +1,14 @@
+module.exports = (req, res) => {
+  const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+      <title>drupalbirthday.fun</title>
+      </head>
+      <body>
+        <p>Soon.</p>
+      </body>
+    </html>
+  `;
+  res.status(200).send(html)
+};
