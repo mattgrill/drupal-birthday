05bd9b52ae5c0b5acee444e2ee1889189ca7c206
Matthew Grill <mattgrill@users.noreply.github.com>
2019-10-29 15:08:31 +0100

    Homepage (#18)

--- api/index.js
+++ api/index.js
@@ -1,12 +1,39 @@
 module.exports = (req, res) => {
   const html = `
-    <!DOCTYPE html>
-    <html>
+    <!doctype html>
+    <html lang="en">
       <head>
-      <title>drupalbirthday.fun</title>
+      <title>Drupal Birthday . Fun</title>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="/screen.css">
+      <meta name="title" content="Drupal Birthday . Fun" />
+      <meta name="description" content="Find your Drupal Birthday, for Fun!" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Drupal Birthday . Fun" />
+      <meta property="og:title" content="Drupal Birthday . Fun" />
+      <meta property="og:description" content="Find your Drupal Birthday, for Fun!" />
+      <meta property="og:url" content="https://drupalbirthday.fun/" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content="Drupal Birthday . Fun" />
+      <meta name="twitter:description" content="Find your Drupal Birthday, for Fun!" />
+      <meta name="twitter:url" content="https://drupalbirthday.fun/" />
+      <script type="application/ld+json">
+        {
+          "@context": "http://schema.org",
+          "@type": "WebSite",
+          "url": "https://drupalbirthday.fun/",
+          "name": "Drupal Birthday . Fun",
+          "potentialAction": {
+            "@type": "SearchAction",
+            "target": "https://drupalbirthday.fun/{query}",
+            "query-input": "required"
+          }
+        }
+      </script>
       </head>
       <body>
-        <p>Soon.</p>
+        <p>https://drupalbirthday.fun/{your drupal.org username}</p>
       </body>
     </html>
   `;
